4b1c2b9225253ec6edf01303394de5ca2813f9b1
joacco22 <joaco.30.04.06@gmail.com>
2024-09-21 15:21:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+const enlace = document.createElement('a');
+enlace.href = 'http://qFQya9FzAXPVDAj:G00THoiWJRw9rAF@localhost:45037/storage/emulated/0/Download/flores.html';
+enlace.textContent = 'Visitar página';
+document.body.appendChild(enlace);
